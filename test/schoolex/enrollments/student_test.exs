00cde60ca534905ea9188fedc078e6b104083d11
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-10-24 07:06:04 -0300

    test: should not enroll without valid student CPF

--- test/schoolex/enrollments/student_test.exs
+++ test/schoolex/enrollments/student_test.exs
@@ -6,7 +6,7 @@ defmodule Schoolex.Enrollments.StudentTest do
 
   describe "changeset/2" do
     test "should not enroll without valid student name" do
-      attrs = %{name: "Ana"}
+      attrs = %{name: "Ana", cpf: "832.081.519-34"}
 
       response = Student.changeset(attrs)
 
@@ -14,6 +14,22 @@ defmodule Schoolex.Enrollments.StudentTest do
 
       assert errors_on(response) == expected_response
       assert %Changeset{valid?: false} = response
+    end
+
+    test "should not enroll without valid student CPF" do
+      invalid_attrs = %{name: "Ana Silva", cpf: "123.456.789-99"}
+      valid_attrs = %{name: "Ana Silva", cpf: "832.081.519-34"}
+
+      invalid_student = Student.changeset(invalid_attrs)
+      valid_student = Student.changeset(valid_attrs)
+
+      expected_invalid_response = %{cpf: ["invalid student CPF"]}
+
+      assert errors_on(invalid_student) === expected_invalid_response
+      assert %Changeset{valid?: false} = invalid_student
+
+      assert %Changeset{changes: %{cpf: "832.081.519-34"}, valid?: true} = valid_student
+      refute errors_on(valid_student) == expected_invalid_response
     end
   end
 end
