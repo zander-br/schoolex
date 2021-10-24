1b549aabef73bd53857da341c0d40bf8feb531af
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-10-24 09:26:05 -0300

    refactor: simplify students tests

--- test/schoolex/enrollments/student_test.exs
+++ test/schoolex/enrollments/student_test.exs
@@ -1,35 +1,43 @@
 defmodule Schoolex.Enrollments.StudentTest do
   use Schoolex.DataCase, async: true
 
+  import Schoolex.Factory
+
   alias Ecto.Changeset
   alias Schoolex.Enrollments.Student
 
   describe "changeset/2" do
-    test "should not enroll without valid student name" do
-      attrs = %{name: "Ana", cpf: "832.081.519-34"}
+    test "when all attrs is valid, retuns a valid changeset" do
+      valid_attrs = build(:student_attrs)
 
-      response = Student.changeset(attrs)
+      valid_student = Student.changeset(valid_attrs)
 
-      expected_response = %{name: ["invalid student name"]}
-
-      assert errors_on(response) == expected_response
-      assert %Changeset{valid?: false} = response
+      assert %Changeset{
+               changes: %{name: "Ana Silva", cpf: "832.081.519-34"},
+               valid?: true
+             } = valid_student
     end
 
-    test "should not enroll without valid student CPF" do
-      invalid_attrs = %{name: "Ana Silva", cpf: "123.456.789-99"}
-      valid_attrs = %{name: "Ana Silva", cpf: "832.081.519-34"}
+    test "when invalid name, returns an invalid changeset" do
+      invalid_attrs = build(:student_attrs, %{name: "Ana"})
 
       invalid_student = Student.changeset(invalid_attrs)
-      valid_student = Student.changeset(valid_attrs)
+
+      expected_invalid_response = %{name: ["invalid student name"]}
+
+      assert errors_on(invalid_student) == expected_invalid_response
+      assert %Changeset{valid?: false} = invalid_student
+    end
+
+    test "when invalid CPF, returns an invalid changeset" do
+      invalid_attrs = build(:student_attrs, %{cpf: "123.456.789-99"})
+
+      invalid_student = Student.changeset(invalid_attrs)
 
       expected_invalid_response = %{cpf: ["invalid student CPF"]}
 
       assert errors_on(invalid_student) === expected_invalid_response
       assert %Changeset{valid?: false} = invalid_student
-
-      assert %Changeset{changes: %{cpf: "832.081.519-34"}, valid?: true} = valid_student
-      refute errors_on(valid_student) == expected_invalid_response
     end
   end
 end
