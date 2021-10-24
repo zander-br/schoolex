5285be6e87a339a7d1a54d59108c5c370bf88660
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-10-24 07:05:46 -0300

    feat: should not enroll without valid student CPF

--- lib/schoolex/enrollments/student.ex
+++ lib/schoolex/enrollments/student.ex
@@ -1,23 +1,32 @@
 defmodule Schoolex.Enrollments.Student do
   use Ecto.Schema
 
+  import Brcpfcnpj.Changeset
   import Ecto.Changeset
 
   schema "students" do
     field :name, :string
+    field :cpf, :string
 
     timestamps()
   end
 
   def changeset(student \\ %__MODULE__{}, attrs) do
     student
-    |> cast(attrs, [:name])
+    |> cast(attrs, [:name, :cpf])
     |> validate_name()
+    |> validate_cpf()
   end
 
   defp validate_name(changeset) do
     changeset
     |> validate_required([:name])
     |> validate_format(:name, ~r/^([A-Za-z]+ )+([A-Za-z])+$/, message: "invalid student name")
+  end
+
+  defp validate_cpf(changeset) do
+    changeset
+    |> validate_required([:cpf])
+    |> validate_cpf(:cpf, message: "invalid student CPF")
   end
 end
