8b7ba256e204ade7759ea7cdecc624fc8ee15d5b
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-10-25 07:37:09 -0300

    feat: add student factory

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,10 +1,20 @@
 defmodule Schoolex.Factory do
   use ExMachina.Ecto, repo: Schoolex.Repo
 
+  alias Schoolex.Enrollments.Student
+
   def student_attrs_factory do
     %{
       name: "Ana Silva",
       cpf: "832.081.519-34"
+    }
+  end
+
+  def student_factory do
+    %Student{
+      name: "Ana Silva",
+      cpf: "832.081.519-34",
+      id: "f62732cd-7b02-4594-8e64-d172299381a1"
     }
   end
 end
