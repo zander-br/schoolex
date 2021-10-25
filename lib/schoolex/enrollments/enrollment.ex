553fef2b8424e1cd8e905a2c180a1d2e83b9bd4a
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-10-25 19:56:22 -0300

    feat: add student_id validation

--- lib/schoolex/enrollments/enrollment.ex
+++ lib/schoolex/enrollments/enrollment.ex
@@ -3,6 +3,7 @@ defmodule Schoolex.Enrollments.Enrollment do
 
   import Ecto.Changeset
 
+  alias Ecto.UUID
   alias Schoolex.Enrollments.Student
 
   @primary_key {:id, :binary_id, autogenerate: true}
@@ -17,5 +18,21 @@ defmodule Schoolex.Enrollments.Enrollment do
   def changeset(enrollment \\ %__MODULE__{}, attrs) do
     enrollment
     |> cast(attrs, [:student_id])
+    |> validate_user_id()
+  end
+
+  defp validate_user_id(changeset) do
+    changeset
+    |> validate_required(:student_id)
+    |> validate_uuid(:student_id)
+  end
+
+  defp validate_uuid(changeset, field, options \\ []) when is_atom(field) do
+    validate_change(changeset, field, fn _, student_id ->
+      case UUID.cast(student_id) do
+        :error -> [{field, options[:message] || "invalid uuid"}]
+        {:ok, _uuid} -> []
+      end
+    end)
   end
 end
