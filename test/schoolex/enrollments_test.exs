f44d31cffbd9f9dad720a4ef3ab9cc1abddda6c0
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-10-26 20:48:54 -0300

    test: when there is invalid params, returns an error

--- test/schoolex/enrollments_test.exs
+++ test/schoolex/enrollments_test.exs
@@ -9,14 +9,25 @@ defmodule Schoolex.EnrollmentsTest do
 
   describe "create_enrollment/1" do
     test "when all params is valid, returns the enrollment" do
-      enrollment_params = build(:enrollment_parms)
+      valid_params = build(:enrollment_parms)
 
-      enrollment = Enrollments.create_enrollment(enrollment_params)
+      enrollment = Enrollments.create_enrollment(valid_params)
 
       %Student{id: student_id} =
         Repo.one(from s in Student, order_by: [desc: s.inserted_at], limit: 1)
 
       assert {:ok, %Enrollment{id: _id, student_id: ^student_id}} = enrollment
+    end
+
+    test "when there is invalid student, returns an error" do
+      invalid_student = build(:student_attrs, %{name: "Ana"})
+      invalid_params = build(:enrollment_parms, %{student: invalid_student})
+
+      {:error, reason} = Enrollments.create_enrollment(invalid_params)
+
+      expected_error_response = %{name: ["invalid student name"]}
+
+      assert errors_on(reason) == expected_error_response
     end
   end
 end
